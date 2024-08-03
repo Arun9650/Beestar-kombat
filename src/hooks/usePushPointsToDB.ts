d7218f82b59fb9b27bf72fd9f089976b62afd037
Arun9650 <arun02580@gmail.com>
2024-08-03 22:59:24 +0530

    chore: Update usePushPointsToDB and AuthProvider for better user configuration handling

--- src/hooks/usePushPointsToDB.ts
+++ src/hooks/usePushPointsToDB.ts
@@ -2,22 +2,24 @@
 
 import { updatePointsInDB } from "@/actions/points.actions";
 import { usePointsStore } from "@/store/PointsStore";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 export const usePushPointsToDB = () => {
   // const name = process.env.NEXT_PUBLIC_TAPPED_POINTS_KEYWORD!;
 
   const { points } = usePointsStore();
+  console.log("🚀 ~ usePushPointsToDB ~ points:", points)
   const user = window.localStorage.getItem("authToken");
-  const push2db = async () => {
+  console.log("🚀 ~ usePushPointsToDB ~ user:", user)
+  const push2db = useCallback(async () => {
     const done = await updatePointsInDB({ points, id: user! });
     console.log({ status: done });
     return done;
-  };
+  }, [points, user]);
 
   useEffect(() => {
     if (points % 100 == 0) {
       const done = push2db();
     }
-  }, [points]);
+  }, [points, push2db]);
 };
