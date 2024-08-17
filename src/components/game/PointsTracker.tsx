96a3d96c4192fc65e40b1c10aaa1db66f591bfe2
Arun9650 <arun02580@gmail.com>
2024-08-17 14:10:41 +0530

    refactor: Update PointsTracker component to update points based on PPH

--- src/components/game/PointsTracker.tsx
+++ src/components/game/PointsTracker.tsx
@@ -1,21 +1,37 @@
-"use client"
+"use client";
 
-import { usePointsStore } from '@/store/PointsStore'
-import Image from 'next/image'
-import React from 'react'
-import { formatNumber } from '../../../utils/formatNumber'
+import { usePointsStore } from "@/store/PointsStore";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { formatNumber } from "../../../utils/formatNumber";
 
 const PointsTracker = () => {
-    const { points } = usePointsStore()
+  const { points, PPH, setPoints } = usePointsStore();
 
-    return (
-        <div className='font-bold text-3xl flex items-center gap-2 text-white'>
-            <span className='text-6xl'>
-                <Image src="/assets/images/dollar-coin.png" height={40} width={40} alt="" />
-            </span>
-            {formatNumber(points)}
-        </div>
-    )
-}
+  useEffect(() => {
+    const pointsPerSecond = PPH / 3600;
 
-export default PointsTracker
+    const interval = setInterval(() => {
+      const updatedPoints = points + pointsPerSecond;
+      setPoints(updatedPoints);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [PPH]);
+
+  return (
+    <div className="font-bold text-3xl flex items-center gap-2 text-white">
+      <span className="text-6xl">
+        <Image
+          src="/assets/images/dollar-coin.png"
+          height={40}
+          width={40}
+          alt=""
+        />
+      </span>
+      {formatNumber(points)}
+    </div>
+  );
+};
+
+export default PointsTracker;
