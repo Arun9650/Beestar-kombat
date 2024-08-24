928e8104bf67c73a4c62abb3a546332176ecb762
Arun9650 <arun02580@gmail.com>
2024-08-24 15:30:45 +0530

    refactor: Update Settings page to delete unnecessary local storage items and set currentTapLeft to 500

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -24,7 +24,7 @@ const Settings = () => {
    const [isDrawerOpen,setIsDrawerOpen] = useState(false);
 
 
-   const {currentTapsLeft, increaseTapsLeft} = usePointsStore()
+   const {currentTapsLeft, increaseTapsLeft, setCurrentTapsLeft} = usePointsStore()
    const {multiClickLevel} = useBoostersStore()
    
    useEffect(() => {
@@ -69,13 +69,14 @@ const Settings = () => {
     window.localStorage.removeItem("refill");
     window.localStorage.removeItem("PPH");  
     window.localStorage.removeItem("currentTapLeft");  
+    window.localStorage.removeItem("lastLoginTime");
+
     const userId = window.localStorage.getItem("authToken");
     setButtonLoading(true);
     const result = await DeleteUser(userId!);
-    console.log("🚀 ~ handleDeleteUser ~ result:", result);
     
-    window.localStorage.removeItem("authToken");
-  
+    window.localStorage.setItem("currentTapLeft", "500"); 
+    setCurrentTapsLeft(500); 
     if (result.success) {
       setButtonLoading(false);
       router.push('/');
