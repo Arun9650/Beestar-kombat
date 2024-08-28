1094c9cd2e44e9e31a347e13b0c90d19e9c2a293
Arun9650 <arun02580@gmail.com>
2024-08-28 23:46:53 +0530

    refactor: Clear local storage after deleting user in Settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -79,6 +79,7 @@ const Settings = () => {
     setButtonLoading(true);
     const result = await DeleteUser(userId!);
     window.localStorage.removeItem("authToken");
+    window.localStorage.clear();
     
     window.localStorage.setItem("currentTapsLeft", "500"); 
     window.localStorage.setItem("energyCapacity", "500"); 
