aadd9992c254d0806e49b9288c5ad57d494439f0
Arun9650 <arun02580@gmail.com>
2024-09-14 01:11:52 +0530

    Refactor: Update back button handling and remove unused dependencies in LoadingScreenProvider component

--- src/providers/LoadingScreenProvider.tsx
+++ src/providers/LoadingScreenProvider.tsx
@@ -1,68 +1,58 @@
 "use client";
 
-import { ReactNode, useEffect, useMemo } from "react";
+import { ReactNode, useEffect } from "react";
 import LoadingScreen from "@/components/LoadingScreen";
 import useLoadingScreenStore from "@/store/loadingScreenStore";
 import { usePointsStore } from "@/store/PointsStore";
 import useUserPointsConfig from "@/hooks/useUserPointsConfig";
-import WebApp from "@twa-dev/sdk";
+import WebApp from "@twa-dev/sdk"
 import { UpdateUser } from "@/actions/user.actions";
-import { BackButton, initSwipeBehavior, initViewport } from "@telegram-apps/sdk";
-import { initBackButton } from "@telegram-apps/sdk";
-import {
-  bindMiniAppCSSVars,
-  bindThemeParamsCSSVars,
-  bindViewportCSSVars,
-  initNavigator, useLaunchParams,
-  useMiniApp,
-  useThemeParams,
-  useViewport,
-} from '@telegram-apps/sdk-react';
+import { BackButton } from "@telegram-apps/sdk";
+import { initBackButton } from '@telegram-apps/sdk';
 const LoadingScreenProvider = ({ children }: { children: ReactNode }) => {
   const { isLoading } = useLoadingScreenStore();
   usePointsStore();
 
-   const [backButton] = initBackButton();
-   const [swipeBehavior] = initSwipeBehavior();
-   
-   swipeBehavior.enableVerticalSwipe();
-   backButton.show();
+ 
+
   
-
-   const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
-
-   useEffect(() => {
-    navigator.attach();
-    return () => navigator.detach();
-  }, [navigator]);
-
-
+  
   useEffect(() => {
-  async  function initTg() {
-      if (typeof window !== "undefined") {
-        const [viewport] = initViewport();
-      const vp = await viewport;
-      if (!vp.isExpanded) {
-        vp.expand(); // will expand the Mini App, if it's not
+    function initTg() {
+      if (typeof window !== 'undefined') {
+        WebApp.ready()
+        WebApp.expand()
+        WebApp.disableVerticalSwipes()
+        WebApp.setHeaderColor('#000000');
+        // WebApp.BackButton.onClick( () => window.history.back() );
+        const [backButton] = initBackButton();
+      backButton.show();
+      backButton.on('click', () => window.history.back());
+  } else {
+    console.log('Telegram WebApp is undefined, retrying…');
+    setTimeout(initTg, 500);
     }
-      } else {
-        console.log("Telegram WebApp is undefined, retrying…");
-        setTimeout(initTg, 500);
-      }
     }
     initTg();
   }, []);
 
+  
+
+
+
   useEffect(() => {
+
+
     const updateUser = async () => {
       const user = window.localStorage.getItem("authToken");
 
       if (user) {
         await UpdateUser(user);
       }
-    };
+    }
 
     updateUser();
+    
   }, [isLoading]);
 
   if (!isLoading) {
